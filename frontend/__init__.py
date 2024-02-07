8cd7239fce11da4b7a89308b1bb3a166b9246e90
jessicasewe <jessicasewe89@gmail.com>
2024-02-07 13:45:48 +0000

    sign up into home page

--- frontend/__init__.py
+++ frontend/__init__.py
@@ -1,13 +1,30 @@
 from flask import Flask;
+from flask_sqlalchemy import SQLAlchemy;
+from os import path
+
+db = SQLAlchemy()
+DB_NAME = "TICKiT.db"
 
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'DUMB'
-    
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    db.init_app(app)
+
     from .views import views
     from .auth import auth
 
     app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')
 
+    from .models import User, Comment
+
+    create_database(app)
+
     return app
+
+def create_database(app):
+    if not path.exists('frontend/' + DB_NAME):
+        with app.app_context():
+            db.create_all()
+        print('Created Database!')
--- frontend/auth.py
+++ frontend/auth.py
@@ -1,4 +1,7 @@
-from flask import Blueprint, render_template, request, flash
+from flask import Blueprint, render_template, request, flash, redirect, url_for
+from .models import User
+from werkzeug.security import generate_password_hash, check_password_hash
+from . import db
 
 auth = Blueprint('auth', __name__)
 
@@ -24,5 +27,10 @@ def sign_up():
         elif len(password) < 7:
             flash('Password must be atleast 7 characters.', category='error')
         else:
+            new_user = User(username=username, email=email, password=generate_password_hash(password, method='pbkdf2:sha256'))
+            db.session.add(new_user)
+            db.session.commit()
             flash('Account created!', category='success')
+            return redirect(url_for('views.tickit'))
+
     return render_template("signup.html")
