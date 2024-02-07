176dd24946247863515ff981fa7bbd84a0caddce
jessicasewe <jessicasewe89@gmail.com>
2024-02-07 17:44:34 +0000

    authenticating email and password for signup and login

--- backend/models.py
+++ backend/models.py
@@ -2,8 +2,8 @@ from app import db
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(50), unique=True, nullable=False)
+    email = db.Column(db.String(50), unique=True, nullable=False)
     password = db.Column(db.String(100), nullable=False)
 
     def __repr__(self):
-        return '<User %r>' % self.username
+        return '<User %r>' % self.email
