0ec368e4aea622d34237e3b20bd95f3c090f5c9f
jessicasewe <jessicasewe89@gmail.com>
2024-02-19 09:52:02 +0000

    changes to the repo done

--- frontend/models.py
+++ frontend/models.py
@@ -3,7 +3,12 @@ from . import db
 from flask_login import UserMixin
 from sqlalchemy.sql import func
 
-#user table
+class Comment(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    data = db.Column(db.String(10000))
+    date = db.Column(db.DateTime(timezone=True), default=func.now())
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(150), unique=True)
@@ -12,7 +17,7 @@ class User(db.Model, UserMixin):
     tickets = db.relationship('Ticket')
     comments = db.relationship('Comment')
 
-#tickets table
+
 class Ticket(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     order_id = db.Column(db.String(100))  
@@ -23,3 +28,5 @@ class Ticket(db.Model):
     description = db.Column(db.String(255)) 
     checkout_url = db.Column(db.String(255))
     user_id = db.Column(db.Integer, ForeignKey('user.id'))
+
+    # user = relationship('User', backref='tickets')
